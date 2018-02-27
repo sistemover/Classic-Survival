a4dc72befa7b9aaf1063c3ce9139d5020e7bc010
sistemover <31515147+sistemover@users.noreply.github.com>
2018-02-27 12:06:38 -0300

    Master: Importación de Assets fundamentales - [dce777d]
    Se importa el paquete: Classic-Survival-[dce777d]

---
+++ Proyecto-Classic-Survival/Assets/Lógica/Manager/GameManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour 
+{
+	#region Singleton
+	public static GameManager instance;
+	void Awake ()
+	{
+		if (instance != null)
+			return;
+		instance = this;
+	}
+	#endregion
+
+	//**************************************************
+
+	void Start () 
+	{
+		
+	}
+	
+	void FixedUpdate()
+	{
+		
+	}
+	void Update () 
+	{
+		
+	}
+}
